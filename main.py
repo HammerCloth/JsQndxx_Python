f2fa3e8c24e69656d722066ed0a4af68bb58da81
siyixiong <60774383+HammerCloth@users.noreply.github.com>
2022-12-07 14:01:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@
 @author:yuzai
 @file:main.py
 @time:2022/09/17
+
 """
 from Qndxx import Qndxx
 import os
 
 if __name__ == '__main__':
+    # 这里需要在github中设置
     laravel_session = os.environ["COOKIE1"]
     qndxx = Qndxx(laravel_session)
     qndxx.login()
